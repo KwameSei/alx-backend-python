c77f9d95d47ebc6949eba657f9a4837c2f2ef77b
KwameSei <nathanielosei92@gmail.com>
2023-04-25 03:49:02 +0000

    Added exception method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -13,10 +13,20 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, nested_map, path, expected):
+    def test_access_nested_map(self, nested_map, path, end_results):
         """Method that tests functions"""
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+        self.assertEqual(access_nested_map(nested_map, path), end_results)
 
-    
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b")
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, end_exception):
+        """Testing invalid inputs that raises exception"""
+        with self.assertRaises(KeyError) as invalid:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(invalid.exception), end_exception)
+
+
 if __name__ == '__main__':
     unittest.main()
